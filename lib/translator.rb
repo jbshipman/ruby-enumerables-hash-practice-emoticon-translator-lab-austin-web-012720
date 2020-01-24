aa8a31b459acd33f115ef9eee1d6d36b394b5c3d
James Shipman <jbshipman@icloud.com>
2020-01-24 17:42:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,8 +16,7 @@ def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
   result = library["get_emoticon"][emoticon]
   
-  result == 
-  # "Sorry, the emoticon was not found" if !result
+  result == result ? result : "Sorry, the emoticon was not found"
 end
 
 def get_english_meaning
