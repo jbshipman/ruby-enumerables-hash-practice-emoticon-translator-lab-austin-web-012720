11108e9a358b1dc4a532e7da8adcd2850818e963
James Shipman <jbshipman@icloud.com>
2020-01-24 17:45:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
   result = library["get_emoticon"][emoticon]
   
-  result 
+  result ? result : 
   
   # if result
   #   result
