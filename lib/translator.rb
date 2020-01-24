36240a6d67b522861944d8d7517a618e9e267b2d
James Shipman <jbshipman@icloud.com>
2020-01-24 17:30:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,7 @@ def load_library(file_path)
   # code goes here
   library = { "get_meaning" => {}, "get_emoticon" => {} }
   YAML.load_file(file_path).map do |meaning, array|
+    english, japanese = array
     
   end 
   
